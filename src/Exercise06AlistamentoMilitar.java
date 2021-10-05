2ef25906cbb5e8dd8d5ce31b3d11fc5e2376cae0
Edvunge <53275378+Edvunge@users.noreply.github.com>
2021-10-06 00:40:01 +0100

    14th  -  fourteenth commit

--- src/Exercise06AlistamentoMilitar.java
+++ src/Exercise06AlistamentoMilitar.java
@@ -6,20 +6,9 @@ public class Exercise06AlistamentoMilitar {
     public static void main(String[] args) {
 
         /*
-        Escreva um programa que leia o ano de nascimento de um rapaz e mostre a sua situação em relação ao
-        alistamento militar. - Se estiver antes dos 18 anos, mostre em quantos anos faltam para o alistamento.
+        Escreva um programa que leia o ano de nascimento de um rapaz e mostre a sua situação em relação ao alistamento militar.
+        - Se estiver antes dos 18 anos, mostre em quantos anos faltam para o alistamento.
         - Se já tiver depois dos 18 anos, mostre quantos anos já se passaram do alistamento.
-
-        23:38 começo da digestao
-
-        idade < 18
-        18 - 13 = 5
-        idadeObrigatoria - idadeUser = result
-
-
-        idade > 18
-        18 - 23 = 5
-        idadeUser - idadeObrigatoria = result
         */
 
         int yearBorn;
@@ -31,25 +20,15 @@ public class Exercise06AlistamentoMilitar {
         System.out.println(" Qual é o seu ano de nascimento:? ");
         yearBorn = input.nextInt();
 
-        /*
-        idade <= 2021 - 1995
-        */
+
         int age = yearCurrent - yearBorn;
 
-        /*   inteiro idade = 1995 - 18   */
-        int resultAge1 = yearBorn - ageMandatory;
-
-
-        /*  inteiro idade = 18 - 1995   */
-        int resultAge2 = yearBorn - ageMandatory ;
-
-
         if (age < ageMandatory){
-
+            int resultAge2 = ageMandatory - age;
             System.out.println(" Alistamento Militar: NÃO podes se inscrever, faltam-te " + resultAge2 + "Anos ");
 
         } else {
-
+            int resultAge1 = age - ageMandatory ;
             System.out.println(" Alistamento Militar: já podes se inscrever, e passaste " + resultAge1 + "Anos " );
 
         }
