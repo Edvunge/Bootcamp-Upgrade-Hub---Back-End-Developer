dd1e136a375919aecb6c05ef47337bcff310665e
Edvunge <53275378+Edvunge@users.noreply.github.com>
2021-10-06 21:58:41 +0100

    16th - sixteen commit

--- src/ExercicioDoisInteiros.java
+++ src/ExercicioDoisInteiros.java
@@ -1,8 +1,35 @@
+import java.util.Scanner;
+
+
+
 public class ExercicioDoisInteiros {
     public static void main(String[] args) {
         /*
-        26) Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando na tela uma das mensagens abaixo:
+        Escreva um algoritmo que leia dois números inteiros e compare-os, mostrando na tela uma das mensagens abaixo:
         - O primeiro valor é o maior - O segundo valor é o maior - Não existe valor maior, os dois são iguais
         */
+
+        double numberOne;
+        double numberTwo;
+
+
+        Scanner input = new Scanner(System.in);
+
+        // esta parte é para leitura de dos numeros pelo teclado.
+        System.out.println(" Insira um numero:? ");
+        numberOne = input.nextDouble();
+
+        System.out.println(" Insira um segundo numero:? ");
+        numberTwo = input.nextInt();
+
+        if(numberOne > numberTwo){
+            System.out.println(" O primeiro valor é o maior ");
+        } else if (numberOne < numberTwo){
+            System.out.println(" O segundo valor é o maior ");
+        } else {
+            System.out.println(" Não existe valor maior, os dois são iguais ");
+        }
+
+
     }
 }
